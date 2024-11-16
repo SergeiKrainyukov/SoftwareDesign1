e216b4a98380314467064d9d36adf7b992d57326
Крайнюков Сергей <sergey.kraynyukov@gazprombank.ru>
2024-11-16 10:12:05 +0300

    Lesson16

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+class Animal {
+    public void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Cat extends Animal {
+//    @Override
+    public void makeSound(int numberOfSounds) {
+        for (int i = 0; i < numberOfSounds; i++) {
+            System.out.println("Meow");
+        }
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cat cat = new Cat();
+        cat.makeSound();
+        cat.makeSound(3);
     }
 }
